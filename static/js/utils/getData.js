88ed983c4e0448734c675d3bef59a4d6c9919fe4
Tomás Salina <salinatomass53@gmail.com>
2022-09-03 20:03:42 -0300

    API url to .env

--- static/js/utils/getData.js
+++ static/js/utils/getData.js
@@ -1,13 +1,13 @@
-export const BASE_API = 'https://salinatomass-api.onrender.com';
+export const BASE_API = process.env.API_URL
 
 export const getData = path => {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch(`${BASE_API}${path}`);
-      const data = await response.json();
-      resolve(data);
+      const response = await fetch(`${BASE_API}${path}`)
+      const data = await response.json()
+      resolve(data)
     } catch (err) {
-      reject(err);
+      reject(err)
     }
-  });
-};
+  })
+}
